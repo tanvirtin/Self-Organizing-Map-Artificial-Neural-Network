f57482394e8389dd719cdf3313e85eb2766a0c1d
tanvirtin <tanvir.tinz@gmail.com>
2017-11-15 01:27:06 -0500

    complete

--- __main__.py
+++ __main__.py
@@ -9,30 +9,39 @@ def displayImages(gridSize, gridData):
 	row = 0
 	col = 0
 
-	for i in range(1, gridSize * 2 + 1):
+	for i in range(1, gridSize * gridSize + 1):
 		plt.subplot(gridSize, gridSize, i)
 		fig = plt.imshow(gridData[row][col])
 		fig.axes.get_xaxis().set_visible(False)
 		fig.axes.get_yaxis().set_visible(False)
-
+		# we increment column index each time in the loop
+		col += 1
+		# if the column index equals the gridSize we know
+		# we reached our limit, we set col to 0 when col hits gridSize
+		# which is the maximum index in the grid on the x axis
+		if (col == gridSize):
+			col = 0
+			row += 1
+			# when we hit maximum gridSize we know we go down the y axis
+			if (row == gridSize):
+				row = 0
 	plt.show()
 
 
 def main():
 	images, labels = filteredMnist()
 
-	som = SOM(20, len(images), len(images[0]), 0.1)
+	gridSize = 20
+	epsilon = 0.1
+	trainingDataSize = len(images)
+	trainingDataDimension = len(images[0])
 
-	# # plt.imshow(som.getWeights(), interpolation='none')
-	# # plt.savefig("initialImage.png")
+	som = SOM(gridSize, trainingDataSize, trainingDataDimension, epsilon)
 
-	# for i in tqdm(range(len(images))):
-	# 	som.train(images, i)
+	for i in tqdm(range(len(images))):
+		som.train(images, i)
 
-	# # plt.imshow(som.getWeights(), interpolation='none')
-	# # plt.savefig("finalImage.png")
-
-
+	displayImages(gridSize, som.getPlottingData())
 
 
 if __name__ == "__main__":
